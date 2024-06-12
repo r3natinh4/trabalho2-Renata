687f0ebd5f71dc1dc375b1c5168fa867b57c539c
r3natinh4 <160494911+r3natinh4@users.noreply.github.com>
2024-06-12 09:30:14 -0300

    Create teste.py

---
+++ teste.py
@@ -0,0 +1,13 @@
+class cadastro
+    def __init__(self, nome, número, datadenascimento, cep): #iniciando uma classe
+    self.nome = nome
+    self.número = número
+    self.datadenascimento = data de nascimento
+    self.cep = cep
+
+cadastro = [] 
+#loop para iniciar cadastro
+
+while true:
+    resposta = input ("deseja adicionar um cadastro? (s/n): ")
+    if resposta.lower()
