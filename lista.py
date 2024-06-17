d856cbe11aa57957c6e4077cdb96f3dbcaa8205b
r3natinh4 <160494911+r3natinh4@users.noreply.github.com>
2024-06-17 11:32:00 -0300

    Create lista.py

---
+++ lista.py
@@ -0,0 +1,12 @@
+frutas_lista = """
+-----------------
+Lista de Frutas
+-----------------
+1. Maça - 7
+2. Manga - 3
+3. Laranja - 2
+4. Banana - 4
+5. Uva - 1
+-----------------
+"""
+print(frutas_lista)
